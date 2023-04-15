ce365bd406c9b888a6fd17d76b8715555c75a9b3
Laranjoo <119254462+Laranjoo@users.noreply.github.com>
2023-04-14 21:28:21 -0300

    Add files via upload

---
+++ Tiles/AkuBlock.cs
@@ -0,0 +1,11 @@
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+
+namespace PenumbraMod.Content.Items.Placeable
+{
+    public class AkuBlock : ModTile
+    {
+       
+    }
+}
